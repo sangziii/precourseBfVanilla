488f88e4fb7a644f0d63d8120086c6b408505ed2
이상진 <sangzin.lee@nhn.com>
2018-08-18 17:43:34 +0900

    [DOM 다루기] #1 아코디언 UI - 기본형 1차 완성 스크립트 수정

--- createUI/#1_accodion/src/action.js
+++ createUI/#1_accodion/src/action.js
@@ -1,27 +1,21 @@
 window.onload = function(){
-    var elTitles = document.getElementsByClassName('title_section');
-    var elTitlesLength = elTitles.length;
-    for (var i = 0; i < elTitlesLength; i++){
-        (function (index){
-            elTitles[index].addEventListener('click', function () {
-                var _this = this;
-                var _thisClassList = _this.classList
-                var hasIsActive = false;
+    function accodianInit(accodianDiv){
+        var liElement = accodianDiv.querySelectorAll('.title_section');
 
-                for (var k = 0; k < elTitlesLength; k++) {
-                    elTitles[k].classList.remove("is_active");
-                }
+        function showPanel(titleItem){
+            (accodianDiv.querySelector('.is_active')) && accodianDiv.querySelector('.is_active').classList.remove('is_active');
+            titleItem.classList.add('is_active');
+        }
 
-                for (var i = 0, length = _thisClassList.length; i < length; i++) {
-                    hasIsActive = false;
-                    if (_thisClassList[i] == "is_active"){
-                        console.log("has is active class!!!")
-                        hasIsActive = true;
-                        break;
-                    }
-                    (!hasIsActive) && _thisClassList.add("is_active");
-                }
-            });
-        })(i);
+        function sendEvent(event){
+            showPanel(event.currentTarget);
+        }        
+        
+        for(var i=0,len=liElement.length; i<len; i++){
+            liElement[i].addEventListener('click', sendEvent);
+        }
+
     }
+
+    accodianInit(document.querySelector('.accordion'));
 }
