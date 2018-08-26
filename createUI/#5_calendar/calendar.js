99b995e5eb7281f0b79916916261d4970ef7a16f
이상진 <sangzin.lee@nhn.com>
2018-08-26 19:02:46 +0900

    [DOM 다루기] #5 달력만들기 - 달별 컬러 추가

--- createUI/#5_calendar/calendar.js
+++ createUI/#5_calendar/calendar.js
@@ -1,13 +1,14 @@
 var todayObj = new Date();
 var year = todayObj.getFullYear();
 var month = todayObj.getMonth() + 1;
+var $calendar = document.querySelector('#calendar');
 var icons = document.getElementById('group_arrowIcon');
 
 function initCalendar(nowYear, nowMonth){
     var isLeapYear;
     var NUM_DAY = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    const NAME_DAY = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
     const MONTH_NAME = ["JANUARY", "FEBUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"];
+    const MONTH_COLOR = ["#9dc6d8", "#00b3ca", "#7dd0b6", "#1d4e89", "#d2b29b", "#ff6860", "#f69256", "#ead98b", "#965251", "#c6cccc", "#4e5c5f", "#513c1f"];
     
     var nowMonthFirstDay = (function getFirstDayName(year, month){
         var dateArg = year + '-' + month + '-1';
@@ -24,20 +25,20 @@ function initCalendar(nowYear, nowMonth){
 
         return date.getDay(); // 1일이 무슨 요일인지 나옴
     })(nowYear, nowMonth);
-
-    // 8월 1일은 수요일 -> NAME_DAY[3] => NAME_DAY[nowMonthFirstDay];
-
-    var $calendar = document.getElementById('calendar'); 
+  
+    var $calender_areaHeader = document.getElementById('calendar_header');
+    var $calender_areaWeek = document.getElementById('calendar_weekdays');
     var $content = document.getElementById('calendar_content');
     var $calendar_headerText = $calendar.getElementsByTagName('h1')[0];
-
     var $div_blank = document.createElement('div');
     $div_blank.classList.add('blank');
     var totalBlank = '';
     var dayNumText;
 
-    // h1에 년.월 텍스트 갱신
+    // h1에 년.월 텍스트 갱신 + 월별 헤더컬러 추가
     $calendar_headerText.innerHTML = "" + MONTH_NAME[nowMonth-1] + " " + nowYear;
+    $calender_areaHeader.style.backgroundColor = MONTH_COLOR[nowMonth-1];
+    $calender_areaWeek.style.color = MONTH_COLOR[nowMonth - 1];
 
     // div.blank 넣기
     for(var i=0; i<nowMonthFirstDay; i++){
@@ -71,12 +72,8 @@ function moveMonth(event){
             month++;
         }
     }
-    console.log("year : " + year);
-    console.log("month : " + month);
-
     initCalendar(year, month);
 }
 
 initCalendar(year, month);
-
 icons.addEventListener('click', moveMonth);
