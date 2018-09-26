d877439aaca58652ec6591c21772223c4bfcd632
이상진 <sangzin.lee@nhn.com>
2018-09-26 16:45:08 +0900

    [알고리즘] 버블정렬 추가

---
+++ algorithm/Sort/CarrayClass.js
@@ -0,0 +1,65 @@
+function Carray(numElements){
+    this.dataStore = [];
+    this.pos = 0;
+    this.numElements = numElements;
+
+    for(var i=0; i<numElements; i++){
+        this.dataStore[i] = i;
+    }
+}
+
+Carray.prototype.setData = function(){
+    for(var i=0; i<this.numElements; i++){
+        this.dataStore[i] = Math.floor(Math.random() * (this.numElements + 1));
+    }
+}
+
+Carray.prototype.clear = function(){
+    for(var i=0; i<this.numElements; i++){
+        this.dataStore[i] = 0;
+    }
+}
+
+Carray.prototype.insert = function(element){
+    this.dataStore[this.pos++] = element;
+}
+
+Carray.prototype.toString = function(){
+    var retstr = '';
+    for(var i=0; i<this.numElements; i++){
+        retstr += this.dataStore[i] + " ";
+        if(i>0 && i%10===0){
+            retstr += "\n";
+        }
+    }
+    return retstr;
+}
+
+Carray.prototype.swap = function(arr, idx1, idx2){
+    var temp = arr[idx1];
+    arr[idx1] = arr[idx2];
+    arr[idx2] = temp;
+}
+
+Carray.prototype.bubbleSort = function () {
+    len = this.dataStore.length;
+
+    for (k = 0; k <= len - 2; k++) {
+        for (var i = 0; i < len - k; i++) {
+            if (this.dataStore[i] > this.dataStore[i + 1]) {
+                this.swap(this.dataStore, i, i + 1);
+            }
+        }
+        console.log(this.toString());
+    }
+}
+
+var myNums = new Carray(10);
+console.log('Before BubbleSort');
+myNums.setData();
+console.log(myNums.toString());
+console.log('After BubbleSort');
+myNums.bubbleSort();
+console.log(myNums.toString());
+
+
